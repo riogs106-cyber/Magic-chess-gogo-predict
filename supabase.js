3c5f752078f64407b3a7f90bb06b4d7c0ab6dd3f
riogs106-cyber <riogs106@gmail.com>
2025-10-15 15:22:11 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = https://wwaokeydohbtzbrxvcqt.supabase.co
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3YW9rZXlkb2hidHpicnh2Y3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MTUyNDMsImV4cCI6MjA3NjA5MTI0M30.lvSM9GCfbsLbp_5SfHTjdZMZlWhpvJ87S1g1zcSygas
+export const supabase = createClient(supabaseUrl, supabaseKey)
